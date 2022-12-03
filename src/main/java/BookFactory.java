09f5f2b5ae98defd63040b3ee5bf2270d3327b95
m1z1na <51057800+m1z1na@users.noreply.github.com>
2022-12-03 17:41:39 +0300

    book

--- src/main/java/BookFactory.java
+++ src/main/java/BookFactory.java
@@ -2,7 +2,7 @@ public class BookFactory {
     private final String  delimeter = ","; // Разделитель
     public Book getBookInfoFromText(String text) {
 
-        String[] data = text.split(",");
+        String[] data = text.split(delimeter);
         Short year = Short.valueOf(data[0]);
         String author = data[1];
         String title = data[2];
